5c9cbaf30ea79317c6e22ee8d86421604f9c1633
Arturo Estrada rodriguez <arturoe@mty.ipointsystems.com>
2020-01-02 17:44:53 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # js-dev-env
+
+This is a personalized boilerplate for JavaScript development for both clients and servers. The goal is to make reduce the JavaScript environment setup time.
---
+++ buildScripts/srcServer.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import config from './../webpack.config.dev';
+
+const port = 35000;
+const app = express();
+const compiler = webpack(config);
+
+app.use(webpackDevMiddleware(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  } else {
+    open('http://localhost:' + port);
+  }
+});
